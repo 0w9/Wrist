2a3adef5976bdfecbeb68f41ba4ac660d8cb0c61
0w9 <79471472+0w9@users.noreply.github.com>
2022-03-08 11:57:16 +0100

    Initial Commit

---
+++ WristMenu/WristMenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  WristMenuApp.swift
+//  WristMenu
+//
+//  Created by Lennard Dorst on 08.03.22.
+//
+
+import SwiftUI
+
+@main
+struct WristMenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
